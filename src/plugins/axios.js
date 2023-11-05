ccabc7306b1603b808aaf2763f4f09602e141ea6
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-11-05 19:22:42 -0400

    Abstracting use axios plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import authToken from "@/utils/auth-token";
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
+
+axios.interceptors.request.use(function (config) {
+    let apiToken = authToken.get();
+    if (apiToken) {
+        config.headers.Authorization = apiToken;
+    }
+    return config;
+});
+
+/**
+ * Call .then() and .catch() and .then()
+ * 
+ * @param {Object} config 
+ * @returns {Promise}
+ */
+const req = async (config = {
+    action: null,
+    method: 'get',
+    data: null,
+    success: null,
+    fail: null,
+    finally: null
+}) => {
+    return axios.request({
+        url: config?.action,
+        method: config?.method,
+        data: config?.data
+    }).then((response) => {
+        if (config?.success) {
+            config.success(response);
+        }
+    }).catch((response) => {
+        console.log(response.response.data.error);
+
+        if (config?.fail) {
+            config.fail(response);
+        }
+    }).then(() => {
+        if (config?.finally) {
+            config.finally();
+        }
+    })
+}
+
+/**
+ * Return a axios.request instance
+ * 
+ * @param {Object} config 
+ * @returns {Promise}
+ */
+const request = async (config = {
+    action: null,
+    method: 'get',
+    data: null
+}) => {
+    return axios.request({
+        url: config?.action,
+        method: config?.method,
+        data: config?.data
+    });
+};
+
+export { req, request };
