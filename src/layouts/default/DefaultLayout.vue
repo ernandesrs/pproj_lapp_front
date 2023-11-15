8743f46d2f5b78e944befa93838c42b72f144931
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-11-15 09:10:28 -0400

    Theme toggler

--- src/layouts/default/DefaultLayout.vue
+++ src/layouts/default/DefaultLayout.vue
@@ -2,8 +2,14 @@
 	<v-layout>
 		<navigation-drawer v-model="navigation.drawer" />
 
-		<v-app-bar border="b" scroll-behavior="hide" scroll-threshold="250" flat density="comfortable" color="white">
+		<v-app-bar border="b" scroll-behavior="hide" scroll-threshold="250" flat density="comfortable">
 			<v-app-bar-nav-icon variant="text" @click.stop="navigation.drawer = !navigation.drawer" />
+
+			<template #append>
+				<v-btn-group density="compact">
+					<v-btn @click="method_toggleTheme" icon="mdi-brightness-6" variant="text" :ripple="false" />
+				</v-btn-group>
+			</template>
 		</v-app-bar>
 
 		<v-main class="py-6 mt-12" style="min-height: 300px;">
@@ -19,12 +25,19 @@ import AlertComp from '@/components/AlertComp.vue';
 import NavigationDrawer from './NavigationDrawer.vue';
 import { useAppStore } from '@/store/app';
 import { reactive } from 'vue';
+import { useTheme } from 'vuetify';
 
 const appStore = useAppStore();
 
 const navigation = reactive({
 	drawer: false
 });
+
+const theme = useTheme();
+
+const method_toggleTheme = () => {
+	theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 
 appStore.startMonitors();
 
