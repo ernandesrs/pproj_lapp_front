7c45a02fa8805fcf4957e48278f4e873bfa55cbf
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-11-17 10:03:44 -0400

    >

--- src/views/default/users/ShowView.vue
+++ src/views/default/users/ShowView.vue
@@ -1,11 +1,51 @@
 <template>
     <base-view page-title="Detalhes"
-        :breadcrumbs="[{ title: 'Usuários', to: { name: 'dashboard.users' } }, { title: 'Ver usuário' }]">
+        :breadcrumbs="[{ title: 'Usuários', to: { name: 'dashboard.users' } }, { title: 'Ver usuário' }]"
+        :loading="data.getting">
+
+        <!-- start -->
+        {{ data.user }}
+        <!-- end -->
+
     </base-view>
 </template>
 
 <script setup>
 
 import BaseView from '@/layouts/default/BaseView.vue';
+import { req } from '@/plugins/requester';
+import { reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+
+const router = useRouter();
+
+const data = reactive({
+    getting: true,
+    user: null
+});
+
+const method_getUser = () => {
+    let userId = route.params.user_id;
+
+    let action = '/admin/users/' + userId;
+
+    return req({
+        action: action,
+        method: 'get',
+        success: (resp) => {
+            data.user = resp.data.user;
+        },
+        fail: () => {
+            router.push({ name: 'dashboard.users' });
+        },
+        finally: () => {
+            data.getting = false;
+        }
+    });
+};
+
+method_getUser();
 
 </script>
