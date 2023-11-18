30853aa3045dff819d00330e0096ee3c97c02fa7
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-11-18 08:44:30 -0400

    Updates in table compojnnet

--- src/views/default/users/ListView.vue
+++ src/views/default/users/ListView.vue
@@ -2,9 +2,9 @@
     <base-view page-title="Usuários" :breadcrumbs="[{ title: 'Usuários', to: { name: 'dashboard.users' } }]"
         :action-create="{ show: true, text: 'Novo usuário', to: { name: 'dashboard.users.create' } }">
 
-        <table-comp
+        <table-comp data-field="users"
             :thumb="{ key: 'photo_url', alternativeKey: 'first_name', label: 'Foto', width: 35, height: 35, circle: true }"
-            :action-get-list="method_getUsers" :action-change-page="method_changePage" :action-filter="method_filterList"
+            :action-get-list="method_getUsers"
             :columns="[{ key: 'first_name', label: 'Nome' }, { key: 'last_name', label: 'Sobrenome' }, { key: 'email', label: 'Email' }]"
             :action-show="(info) => { return { name: 'dashboard.users.show', params: { user_id: info.id } } }"
             :action-edit="(info) => { return { name: 'dashboard.users.edit', params: { user_id: info.id } } }"
@@ -18,29 +18,18 @@
 import BaseView from '@/layouts/default/BaseView.vue';
 import TableComp from '@/components/TableComp.vue';
 import { request } from '@/plugins/requester';
-import setting from '@/utils/setting';
 
-const method_getUsers = (d) => {
-    let action = '/admin/users?limit=' + setting.getSetting('items_per_page');
+const method_getUsers = (urlData) => {
+    let action = '/admin/users';
 
-    if (d?.urlParams) {
-        action += '&' + d.urlParams;
+    if (urlData?.urlParams) {
+        action += '?' + urlData.urlParams;
     }
 
     return request({
         action: action,
         method: 'get'
     });
-};
-
-const method_changePage = (page) => {
-    return method_getUsers({
-        urlParams: 'page=' + page
-    });
-};
-
-const method_filterList = (filterData) => {
-    return method_getUsers(filterData);
 };
 
 </script>
