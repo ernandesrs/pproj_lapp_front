6a042592d3f5bee8582cca71a4a4c25131e42384
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-07 20:34:41 -0400

    Section component

---
+++ src/components/SectionComp.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card :elevation="props.elevation" :border="props.border" :loading="props.loading">
+        <v-card-title v-if="props.title">{{ props.title }}</v-card-title>
+        <v-card-subtitle v-if="props.subtitle">{{ props.subtitle }}</v-card-subtitle>
+        <v-card-text>
+            <slot name="content" />
+        </v-card-text>
+        <v-card-actions class="d-flex justify-end">
+            <slot name="actions" />
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    elevation: {
+        type: [Number, String],
+        default: 0
+    },
+    border: {
+        type: [Boolean, Number, String],
+        default: false
+    },
+
+    title: {
+        type: [String],
+        default: null
+    },
+    subtitle: {
+        type: [String],
+        default: null
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    }
+});
+
+</script>
--- src/views/default/HomeView.vue
+++ src/views/default/HomeView.vue
@@ -1,11 +1,117 @@
 <template>
   <base-view page-title="Resumo geral" :breadcrumbs="[]">
-    <h1>HOME</h1>
+
+    <v-row>
+      <v-col cols="12">
+        <section-comp title="Seção de dashboard" subtitle="Breve descrição da seção de dashboard">
+          <template #content>
+            <v-table hover density="compact">
+              <thead>
+                <tr>
+                  <th>Col #1</th>
+                  <th>Col #2</th>
+                  <th>Col #3</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>A</td>
+                  <td>B</td>
+                  <td>C</td>
+                </tr>
+                <tr>
+                  <td>D</td>
+                  <td>E</td>
+                  <td>F</td>
+                </tr>
+                <tr>
+                  <td>G</td>
+                  <td>H</td>
+                  <td>I</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </template>
+        </section-comp>
+      </v-col>
+
+      <v-col cols="12" md="6">
+        <section-comp title="Seção de dashboard" subtitle="Breve descrição da seção de dashboard">
+          <template #content>
+            <v-table hover density="compact">
+              <thead>
+                <tr>
+                  <th>Col #1</th>
+                  <th>Col #2</th>
+                  <th>Col #3</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>A</td>
+                  <td>B</td>
+                  <td>C</td>
+                </tr>
+                <tr>
+                  <td>D</td>
+                  <td>E</td>
+                  <td>F</td>
+                </tr>
+                <tr>
+                  <td>G</td>
+                  <td>H</td>
+                  <td>I</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </template>
+
+          <template #actions>
+            <v-btn color="primary" variant="text" text="Ver tudo" />
+          </template>
+        </section-comp>
+      </v-col>
+
+      <v-col cols="12" md="6">
+        <section-comp border title="Seção de dashboard" subtitle="Breve descrição da seção de dashboard">
+          <template #content>
+            <v-table hover density="compact">
+              <thead>
+                <tr>
+                  <th>Col #1</th>
+                  <th>Col #2</th>
+                  <th>Col #3</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>A</td>
+                  <td>B</td>
+                  <td>C</td>
+                </tr>
+                <tr>
+                  <td>D</td>
+                  <td>E</td>
+                  <td>F</td>
+                </tr>
+                <tr>
+                  <td>G</td>
+                  <td>H</td>
+                  <td>I</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </template>
+        </section-comp>
+      </v-col>
+    </v-row>
+
   </base-view>
 </template>
 
 <script setup>
 
+import SectionComp from '@/components/SectionComp.vue';
 import BaseView from '@/layouts/default/BaseView.vue';
 
 </script>
