53734ef0d554d4c1afe1eeec2bf5f3073b252117
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-09 14:12:57 -0400

    Alert: error names

--- lappf.config.js
+++ lappf.config.js
@@ -84,67 +84,6 @@ const config = {
 
     /**
      * 
-     * Backend errors names
-     * 
-     */
-    messages: {
-        /**
-         * 
-         * All
-         * 
-         */
-        'NotFoundException': {
-            title: 'Não encontrado',
-            text: 'A url ou recurso que você solicitou não foi encontrado.'
-        },
-        'InvalidDataException': {
-            title: 'Dados inválidos',
-            text: 'Um ou mais dados informados são inválidos.'
-        },
-        'UnauthorizedActionException': {
-            title: 'Não autorizado',
-            text: 'Desculpe, você não tem autorização para executar esta ação.'
-        },
-
-        /**
-         * 
-         * Admin
-         * 
-         */
-        'HasDependentsException': {
-            title: 'Impossível excluir',
-            text: 'Este recurso não pode ser excluído pois há outros recursos vinculados a ele.'
-        },
-
-        /**
-         * 
-         * Account
-         * 
-         */
-        'InvalidRegisterVerificationTokenException': {
-            title: 'Token inválido',
-            text: 'O token de verificação de registro é inválido.'
-        },
-        'HasAlreadyBeenVerifiedException': {
-            title: 'Já verificado',
-            text: 'A sua conta já foi verificada.'
-        },
-        'UnauthenticatedException': {
-            title: 'Não autenticado',
-            text: 'Você não está autenticado.'
-        },
-        'EmailOrPasswordInvalidException': {
-            title: 'Falha no login',
-            text: 'O e-mail e/ou senha informado é inválido.'
-        },
-        'InvalidUpdatePasswordTokenException': {
-            title: 'Token inválido',
-            text: 'O token de atualização de senha é inválido.'
-        }
-    },
-
-    /**
-     * 
      * *
      * 
      */
--- src/store/alert.js
+++ src/store/alert.js
@@ -1,7 +1,71 @@
 // Utilities
 import { defineStore } from 'pinia'
 import cookie from '@/plugins/cookie';
-import config from './../../lappf.config';
+
+/**
+ * 
+ * Backend errors names
+ * 
+ */
+const errorNames = {
+    /**
+     * 
+     * All
+     * 
+     */
+    'UnexpectedError': {
+        title: 'Erro',
+        text: 'Desculpe, houve um erro inexperado. Entre em contato.'
+    },
+    'NotFoundException': {
+        title: 'Não encontrado',
+        text: 'A url ou recurso que você solicitou não foi encontrado.'
+    },
+    'InvalidDataException': {
+        title: 'Dados inválidos',
+        text: 'Um ou mais dados informados são inválidos.'
+    },
+    'UnauthorizedActionException': {
+        title: 'Não autorizado',
+        text: 'Desculpe, você não tem autorização para executar esta ação.'
+    },
+
+    /**
+     * 
+     * Admin
+     * 
+     */
+    'HasDependentsException': {
+        title: 'Impossível excluir',
+        text: 'Este recurso não pode ser excluído pois há outros recursos vinculados a ele.'
+    },
+
+    /**
+     * 
+     * Account
+     * 
+     */
+    'InvalidRegisterVerificationTokenException': {
+        title: 'Token inválido',
+        text: 'O token de verificação de registro é inválido.'
+    },
+    'HasAlreadyBeenVerifiedException': {
+        title: 'Já verificado',
+        text: 'A sua conta já foi verificada.'
+    },
+    'UnauthenticatedException': {
+        title: 'Não autenticado',
+        text: 'Você não está autenticado.'
+    },
+    'EmailOrPasswordInvalidException': {
+        title: 'Falha no login',
+        text: 'O e-mail e/ou senha informado é inválido.'
+    },
+    'InvalidUpdatePasswordTokenException': {
+        title: 'Token inválido',
+        text: 'O token de atualização de senha é inválido.'
+    }
+};
 
 export const useAlertStore = defineStore('alert', {
     state: () => ({
@@ -19,15 +83,15 @@ export const useAlertStore = defineStore('alert', {
         addError(error, flash = false, duration = null) {
             if (flash) {
                 this.addFlashMessage(
-                    config.messages[error]?.text ?? 'Desculpe, houve um erro inesperado.',
-                    config.messages[error]?.title ?? 'Erro!',
+                    errorNames[error]?.text ?? errorNames['UnexpectedError'].text,
+                    errorNames[error]?.title ?? errorNames['UnexpectedError'].title,
                     'danger',
                     duration
                 );
             } else {
                 this.addMessage(
-                    config.messages[error]?.text ?? 'Desculpe, houve um erro inesperado.',
-                    config.messages[error]?.title ?? 'Erro!',
+                    errorNames[error]?.text ?? errorNames['UnexpectedError'].text,
+                    errorNames[error]?.title ?? errorNames['UnexpectedError'].title,
                     'danger',
                     duration
                 );
